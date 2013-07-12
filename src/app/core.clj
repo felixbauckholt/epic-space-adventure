6e69f40658d3280cad5b4265f3ce63c98f5e0861
felix <felixbauckholt AT gmail PUNKT com>
2013-07-12 22:13:46 +0200

    Besseres Loggen (+lein2)

--- src/app/core.clj
+++ src/app/core.clj
@@ -1,5 +1,11 @@
 (ns app.core)
 
+(.setLevel (java.util.logging.Logger/getLogger "")  (java.util.logging.Level/WARNING))
+
+(let [logger (java.util.logging.Logger/getLogger "debug")
+      level  (java.util.logging.Level/WARNING)]
+  (defn log [msg] (.log logger level (str msg))))
+
 (defn -main []
   (.start (proxy [com.jme3.app.SimpleApplication] []
     ; Callbacks hier:
