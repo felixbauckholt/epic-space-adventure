79b0ddbb9297e7582450719c85b1906a440a261b
felix <felixbauckholt AT gmail PUNKT com>
2013-07-12 18:51:12 +0200

    Zusammenarbeit mit jMonkey!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
 (defproject app "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :url "http://github.com/felixbauckholt/epic-space-adventure"
+  :dependencies [
+    [org.clojure/clojure "1.3.0"]
+    [org.cogchar/ext.bundle.opengl.jmonkey "1.0.6.2"]]
+  :main app.core)
--- src/app/core.clj
+++ src/app/core.clj
@@ -1,1 +1,7 @@
 (ns app.core)
+
+(defn -main []
+  (.start (proxy [com.jme3.app.SimpleApplication] []
+    ; Callbacks hier:
+    (simpleInitApp [] (print "init!"))
+  )))
